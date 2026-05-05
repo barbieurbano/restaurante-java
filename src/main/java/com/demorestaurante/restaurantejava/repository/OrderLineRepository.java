f055e6cc69b931d62df560904dff3c258095c4d1
Barbara Urbano <sbaarby@gmail.com>
2026-05-05 11:01:32 +0200

    Se agrega la query

--- src/main/java/com/demorestaurante/restaurantejava/repository/OrderLineRepository.java
+++ src/main/java/com/demorestaurante/restaurantejava/repository/OrderLineRepository.java
@@ -2,11 +2,16 @@ package com.demorestaurante.restaurantejava.repository;
 
 import com.demorestaurante.restaurantejava.model.OrderLine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface OrderLineRepository extends JpaRepository<OrderLine, Long> {
     List<OrderLine> findByOrder_Id(Long id);
-
+    @Query("""
+    SELECT SUM(ol.quantity * ol.dish.price)
+    FROM OrderLine ol WHERE ol.order.id = ?1
+    """)
+    Double calculateTotalPrice(Long orderId);
 
 }
